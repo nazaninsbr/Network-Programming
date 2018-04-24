99c56cd9a7529a7f9a892180af33263ca682c8a8
ZibaO <ziba091962912@gmail.com>
2018-04-24 12:38:55 +0430

    handshaking

--- TCPServerSocketImpl.java
+++ TCPServerSocketImpl.java
@@ -29,22 +29,33 @@ public class TCPServerSocketImpl extends TCPServerSocket {
             System.out.println("RECEIVED: " + sentence);
             InetAddress IPAddress = receivePacket1.getAddress();
             port = receivePacket1.getPort();
-            if(sentence="SYNC")
-
-            
-           
-
-            //**************//
-       
-            DatagramPacket receivePacket2 = new DatagramPacket(receiveData, receiveData.length);
-            this.socket.receive(receivePacket2);
+            String[] splited = sentence.split("\\s+");
+            new_seq_No = Integer.parseInt(splited[1]);
+            if(splited[0]=="SYN")
             {
-                this.seq_No +=1;
-                String ackNoString = Integer.toString(seq_No);
-                sentence= ackNoString + sentence;
-                sendData = sentence.getBytes();
+                this.ack_No=new_seq_No+1;
+                String ackNoString = Integer.toString(this.ack_No);
+                String seqNoString = Integer.toString(this.seq_No);
+                String sentence_for_send = "SYN-ACK" + " "+seqNoString+" "+ackNoString;
+                sendData = sentence_for_send.getBytes();
                 DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
                 this.socket.send(sendPacket);
+
+            }
+
+            //**************/
+            DatagramPacket receivePacket2 = new DatagramPacket(receiveData, receiveData.length);
+            this.socket.receive(receivePacket2);
+            String sentence = new String(receivePacket2.getData());
+            System.out.println("RECEIVED: " + sentence);
+            String[] splited = sentence.split("\\s+");
+
+            if(split[0]=="ACK")
+
+            {
+                this.seq_No = Integer.parseInt(splited[1])+1;
+                this.ack_No = Integer.parseInt(splited[2])+1;
+
             }
             someOneIsConnected = 0;
         }
