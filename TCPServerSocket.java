c33bb842c12a91d42c0824cc9f91ffa50ba1a129
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-04-24 11:22:13 +0430

    one more error for port

--- TCPServerSocket.java
+++ TCPServerSocket.java
@@ -1,30 +1,7 @@
 public abstract class TCPServerSocket {
-	public TCPServerSocket(int port) throws Exception {
-		 EnhancedDatagramSocket socket= new EnhancedDatagramSocket(port);
-	}
+	public TCPServerSocket(int port) throws Exception {}
 
-	public abstract TCPSocket accept() throws Exception{
-		//needs to have handshaking
-		int notConnected = 1;
-		int port;
-		while(notConnected==1){
-			byte[] receiveData = new byte[1024];
-			byte[] sendData = new byte[1024];
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			this.socket.receive(receivePacket);
-			String sentence = new String( receivePacket.getData());
-			System.out.println("RECEIVED: " + sentence);
-			InetAddress IPAddress = receivePacket.getAddress();
-			port = receivePacket.getPort();
-			sendData = sentence.getBytes();
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-			this.socket.send(sendPacket);
-			DatagramPacket receivePacket = new DatagramPacket(sentence, sentence.length);
-			this.socket.receive(receivePacket);
-			notConnected = 0;
-		}
-		ServerSocket tcp_socket = new ServerSocket(port);
-	}
+	public abstract TCPSocket accept() throws Exception;
 
 	public abstract void close() throws Exception;
 }
